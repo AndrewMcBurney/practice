cb5451c2c3e60cec5a884dc2e4c800d74c09caac
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2018-05-08 18:35:18 -0400

    Simplified index computing function, and added documentation to `is_valid_sudoku` exercise.

--- ruby/src/is_valid_sudoku.rb
+++ ruby/src/is_valid_sudoku.rb
@@ -10,20 +10,22 @@ def is_valid_sudoku(board)
 
   # Runtime: O(n^2)
   #
-  # Checks validity amongst rows and colums
+  # Checks validity amongst rows, colums, and the current 3x3 square
   board.each.with_index do |row, row_index|
     rows[row_index] = {}
 
     row.each.with_index do |val, col_index|
-      square_index = compute_square_index(row_index, col_index)
+      # Ignore blank values
+      next if val == "."
 
+      square_index = compute_square_index(row_index, col_index)
       square[square_index] ||= {}
       cols[col_index] ||= {}
 
-      next if val == "."
-      return false if rows[row_index][val] || cols[col_index][val] ||
-                      square[square_index][val]
+      # Return if a value has been used twice in the row, column, or current 3x3 square
+      return false if rows[row_index][val] || cols[col_index][val] || square[square_index][val]
 
+      # This value can not be reused in the row, column, or current 3x3 square
       rows[row_index][val] = true
       cols[col_index][val] = true
       square[square_index][val] = true
@@ -34,33 +36,5 @@ def is_valid_sudoku(board)
 end
 
 def compute_square_index(row_index, col_index)
-  case row_index
-  when 0..2
-    case col_index
-    when 0..2
-      0
-    when 3..5
-      1
-    when 6..8
-      2
-    end
-  when 3..5
-    case col_index
-    when 0..2
-      3
-    when 3..5
-      4
-    when 6..8
-      5
-    end
-  when 6..8
-    case col_index
-    when 0..2
-      6
-    when 3..5
-      7
-    when 6..8
-      8
-    end
-  end
+  3 * (row_index / 3).floor + (col_index / 3).floor
 end
