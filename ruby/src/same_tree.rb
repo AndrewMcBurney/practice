e96b5baede9d46a251e1abcc620530dab79fb6a2
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2018-05-27 22:17:27 -0400

    Added `is_same_tree` problem.

---
+++ ruby/src/same_tree.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Definition for a binary tree node.
+# class TreeNode
+#   attr_accessor :val, :left, :right
+#   def initialize(val)
+#     @val = val
+#     @left, @right = nil, nil
+#   end
+# end
+
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+def is_same_tree(p, q)
+  return true if p.nil? && q.nil?
+  return false if p&.val != q&.val
+  is_same_tree(p.left, q.left) && is_same_tree(p.right, q.right)
+end
